2c9bdff83528bee594a362226c2866076cf9fe22
iranga <irangacomz@gmail.com>
2021-03-09 22:45:55 +0530

    Implementation for Insertion Sort

---
+++ src/com/imm/InsertionSort.java
@@ -0,0 +1,30 @@
+package com.imm;
+
+public class InsertionSort {
+    //Instead of swapping items, shift items to right if current > others
+    //Insert in correct position named this Insertion Sort algorithm
+
+    // j=i-1  i
+    // |  ____|
+    // | |
+    //{8,2,4,1,3}  i=1,j=0(8) current(i)=2 8(j) > 2(current) ->shift {8,8,4,1,3} a[j+1]=current -> shift{2,8,4,1,3}
+    //   j i
+    //   | |
+    //{2,8,4,1,3}  i=2,j=1 current(i)=8 8(j) > 4(current) ->shift {2,8,8,1,3} a[j+1]=current -> shift{2,4,8,1,3}
+    //   2=j i=3                     while{ array[j+1] = array[j]             (j-- =1)j(4) > (1)current ->  (j-- =0)j(2) > (1)current  EndOfWile}           a[j]==current
+    //     | |                                                   *                    |                             |                                       |
+    //{2,4,8,1,3}  current(i)=1 8(j) > 1(current) ->shift {2,4,8,8,3}      ->      {2,4,8,8,3}  ->            shift{2,4,4,8,3} -->   shift{4,2,4,8,3} ->   {1,2,4,8,3}
+    public static void sort(int[] array){                                  //             |                               |
+        for(int i=0; i<array.length; i++){                                 //             i                               i
+            var current = array[i];
+            var j = i-1; //previous index
+            while(j >=0 && array[j] > current){ //j canot be negetive value and previous > current
+
+                array[j+1] = array[j];//now shift items to right
+                j--;
+            }
+            array[j+1] = current;
+
+        }
+    }
+}
--- src/com/imm/Main.java
+++ src/com/imm/Main.java
@@ -6,14 +6,16 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-	    int [] array = {3,5,6,7,8,2,1,3,-1};
+	    int [] array = {1,2,0,2};
 
-        BubbleSort.sort(array);
+//        BubbleSort.sort(array);
+//        System.out.println(Arrays.toString(array));
+//
+//        SelectionSort.sort(array);
+//        System.out.println(Arrays.toString(array));
+
+        InsertionSort.sort(array);
         System.out.println(Arrays.toString(array));
-
-        SelectionSorts.sort(array);
-        System.out.println(Arrays.toString(array));
-
 
     }
 
