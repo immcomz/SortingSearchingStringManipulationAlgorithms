f4aa86853d1d9bbb7aab48334e8792497f249015
iranga <irangacomz@gmail.com>
2021-04-06 17:07:43 +0530

    Implementation for Ternary Search

--- src/com/imm/Search.java
+++ src/com/imm/Search.java
@@ -9,6 +9,7 @@ public class Search {
 
         return -1; //otherwise reurn -1
     }
+
     public  static int binarySearchRecursive(int[] array,int target){
         //before binary search need to sort array in ascending order
         return binarySearchRecursive(array,target,0,array.length-1);
@@ -20,7 +21,7 @@ public class Search {
         //   1,2,3,4,5,6,7,8,9]
         //assume target 2
 
-         if(right < left) return -1; // base condition when array is empty
+         if(right < left) return -1; // base condition when partition array is empty
 
          var middle = (left + right)/2;
 
@@ -54,5 +55,36 @@ public class Search {
             else
                 left = middle + 1; // target at the right
         }
+        return -1;
+    }
+
+    public int ternarySearch(int[] array, int target) {
+
+        // Runtime complexity is higher in Ternary Search than binary search
+        return ternarySearch(array, target, 0, array.length - 1);
+    }
+    private int ternarySearch(int[] array, int target, int left, int right) {
+
+        if (left > right)// left become grater than right / target at out of range in array/ there is no such a element in an array
+            return -1;
+
+        int partitionSize = (right - left) / 3;
+        int mid1 = left + partitionSize;
+        int mid2 = right - partitionSize;
+
+        if (array[mid1] == target)
+            return mid1;
+
+        if (array[mid2] == target)
+            return mid2;
+
+        if (target < array[mid1])
+            return ternarySearch(array, target, left, mid1 - 1);
+
+        if (target > array[mid2])
+            return ternarySearch(array, target, mid2 + 1, right);
+
+        //result in between mid1 and mid 2
+        return ternarySearch(array, target, mid1 + 1, mid2 - 1);
     }
 }
